dcff5dca06ce395c0d40cff95ddad5484ad59570
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2021-07-05 20:35:14 +0200

    Add GIF animated to frames_selector.py

--- frames_selector.py
+++ frames_selector.py
@@ -8,19 +8,27 @@ import io
 
 # First the window layout in 2 columns
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
+
+IMG_SIZE = 518
 
 file_list_column = [
   [
     sg.Text("Input Frame Folder"),
     sg.In('img', size=(25, 1), enable_events=True, key="-FOLDER-IN-"),
-    sg.FolderBrowse(initial_folder='img'),
+    sg.FolderBrowse(initial_folder='./'),
   ],
   [
     sg.Text("Output Frame Folder"),
     sg.In('output', size=(25, 1), enable_events=True, key="-FOLDER-OUT-"),
-    sg.FolderBrowse(initial_folder='output'),
+    sg.FolderBrowse(initial_folder='./'),
+  ],
+  [
+    sg.Text("Next scene number"),
+    sg.In('1', size=(25, 1), enable_events=True, key="-SCENE-"),
+  ],
+  [
+    sg.Text("Current frame"),
+    sg.Text("0", key="-CURR-FRAME-", size=(10, 1)),
   ],
   [
     sg.Button("Start", key="-START-")
@@ -34,13 +42,13 @@ file_list_column = [
 image_viewer_column = [
   [sg.Text("First frame")],
   [sg.Text(size=(40, 1), key="-TOUT-1-")],
-  [sg.Image(key="-IMAGE-1-", size=(256, 256))],
+  [sg.Image(key="-IMAGE-1-", size=(IMG_SIZE, IMG_SIZE))],
 ]
 
 image_viewer_column2 = [
-  [sg.Text("Last frame")],
+  [sg.Text("More frames")],
   [sg.Text(size=(40, 1), key="-TOUT-2-")],
-  [sg.Image(key="-IMAGE-2-", size=(256, 256))],
+  [sg.Image(key="-IMAGE-2-", size=(IMG_SIZE, IMG_SIZE))],
 ]
 
 # ----- Full layout -----
@@ -87,18 +95,28 @@ def move_images(root, files, output_dir):
     shutil.move(os.path.join(root, files[idx]), os.path.join(output_root, files[idx]))
 
 
+def extract_scene_number(root):
+  scene_folder = root.split(os.sep)[-1]
+  scene_folder = ''.join(c for c in scene_folder if c.isdigit())
+  return int(scene_folder)
+
+
 if __name__ == '__main__':
   input_folder = os.path.join(os.path.curdir, 'img')
   output_folder = os.path.join(os.path.curdir, 'output')
   root, files = None, None
   g = None
+  i = 0
+  next_scene = 1
   while True:
-    event, values = window.read()
+    event, values = window.read(timeout=75)
     # Folder name was filled in, make a list of files in the folder
     if event == "-FOLDER-IN-":
       input_folder = values["-FOLDER-IN-"]
     elif event == "-FOLDER-OUT-":
       output_folder = values["-FOLDER-OUT-"]
+    elif event == "-SCENE-":
+      next_scene = int('0'+''.join(c for c in values["-SCENE-"] if c.isdigit()))
     elif event == "-START-":
       print('Start with input:',input_folder, 'and output:', output_folder)
       g = navigate_scenes(input_folder)
@@ -106,21 +124,23 @@ if __name__ == '__main__':
       window["-START-"].update(disabled=True)
     elif event == "-NEXT-":
       root, files = next(g, (None, None))
+      scene_n = extract_scene_number(root)
+      if scene_n >= next_scene:
+        next_scene = scene_n + 1
+      else:
+        window.write_event_value('-NEXT-', values)
+        continue
+      window["-CURR-FRAME-"].update(value=f'{next_scene}')
       if root is None:
         window.close()
         break
       files = sorted(files)
       image = cv2.imread(os.path.join(root, files[0]))
-      image = cv2.resize(image, dsize=(256, 256))
+      image = cv2.resize(image, dsize=(IMG_SIZE, IMG_SIZE))
       _, buffer = cv2.imencode(".png", image)
       window["-TOUT-1-"].update(files[0])
       window["-IMAGE-1-"].update(data=base64.b64encode(buffer))
 
-      image = cv2.imread(os.path.join(root, files[-1]))
-      image = cv2.resize(image, dsize=(256, 256))
-      _, buffer = cv2.imencode(".png", image)
-      window["-TOUT-2-"].update(files[-1])
-      window["-IMAGE-2-"].update(data=base64.b64encode(buffer))
     elif event is not None and (event.startswith('Right') or event == 'd' or event == 'y'):
       if root is not None and files is not None:
         print('Images accepted!')
@@ -130,6 +150,13 @@ if __name__ == '__main__':
       if root is not None:
         print('Images rejected!')
         window.write_event_value('-NEXT-', values)
+    elif files is not None and event == "__TIMEOUT__":
+      i = (i+1)%len(files)
+      image = cv2.imread(os.path.join(root, files[i]))
+      image = cv2.resize(image, dsize=(IMG_SIZE, IMG_SIZE))
+      _, buffer = cv2.imencode(".png", image)
+      window["-TOUT-2-"].update(files[i])
+      window["-IMAGE-2-"].update(data=base64.b64encode(buffer))
     if event == "Exit" or event == sg.WIN_CLOSED:
       window.close()
       break
