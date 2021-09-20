b4b4b84b5d4da190e3dc0b0c02b5ea6ef72ecc45
ldeluigi <lucadelu97@gmail.com>
2021-09-20 10:27:47 +0200

    Remove glob patterns

--- movies_to_frames.py
+++ movies_to_frames.py
@@ -1,20 +1,14 @@
-import glob
+import argparse
 import os
-import pstats
+import os.path
 import shutil
 
-import cv2
-import os.path
-import argparse
-
-
-from scenedetect import VideoManager, ContentDetector, StatsManager
 from scenedetect import SceneManager
+from scenedetect import VideoManager, ContentDetector
 from scenedetect.scene_manager import save_images
-from tqdm import tqdm
 
 
-def find_scenes(video_path, output_dir='output'):
+def find_scenes(video_path, output_dir='output', frames_per_scene=3):
   video_manager = VideoManager([video_path])
 
   scene_manager = SceneManager()
@@ -33,40 +27,43 @@ def find_scenes(video_path, output_dir='output'):
   save_images(
     scene_list=scene_list,
     video_manager=video_manager,
-    num_images=10,
+    num_images=frames_per_scene,
     output_dir=output_dir,
     show_progress=True,
     image_name_template='Scene-$SCENE_NUMBER/Frame-$IMAGE_NUMBER')
 
 
-def analyze_video(path_to_video, output_dir):
+def analyze_video(path_to_video, output_dir, frames_per_scene):
   print(f'Analize video {path_to_video}...')
   print('Searching for scenes...')
-  find_scenes(path_to_video, output_dir=output_dir)
+  find_scenes(path_to_video, output_dir=output_dir, frames_per_scene=frames_per_scene)
   print(f'\nDone {path_to_video}!')
 
 
-def parse_folder(folder, output_dir):
+def parse_folder(folder, output_dir, frames_per_scene):
   if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-  video_file_names = glob.glob(folder)
-  for v in video_file_names:
-    folder_name, _ = os.path.splitext(os.path.basename(v))
-    folder_path = os.path.join(output_dir, folder_name)
-    if not os.path.exists(folder_path):
-      os.makedirs(folder_path)
-    else:
-      for f in os.listdir(folder_path):
-        shutil.rmtree(os.path.join(folder_path, f))
-    analyze_video(v, folder_path)
+  for root, dirs, files in os.walk(folder):
+    for v in files:
+      if v.endswith('.mp4') or v.endswith('.mkv'):
+        rel_path = os.path.relpath(root, folder)
+        dest = os.path.join(output_dir, rel_path)
+        src = os.path.join(root, v)
+        if not os.path.exists(dest):
+          os.makedirs(dest)
+        else:
+          for f in os.listdir(dest):
+            shutil.rmtree(os.path.join(dest, f))
+        analyze_video(src, dest, frames_per_scene)
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-i', type=str, help='Input folder')
   parser.add_argument('-o', type=str, help='Output folder', default='img')
-  print(parser.parse_args().i, parser.parse_args().o)
-  parse_folder(parser.parse_args().i, parser.parse_args().o)
+  parser.add_argument('-n', type=int, help='Frames per scene', default=3)
+  print(parser.parse_args().i, parser.parse_args().o, parser.parse_args().n)
+  parse_folder(parser.parse_args().i, parser.parse_args().o, parser.parse_args().n)
 
 
 if __name__ == '__main__':
