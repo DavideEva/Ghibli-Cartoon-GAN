4a9117a509e93d4aa2081ac81f9886efb6e53f7a
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2021-10-12 10:10:26 +0200

    Update frames_selector.py

--- frames_selector.py
+++ frames_selector.py
@@ -1,5 +1,8 @@
 import base64
 import os.path
+import re
+from itertools import groupby
+from operator import itemgetter
 import shutil
 
 import PySimpleGUI as sg
@@ -7,6 +10,8 @@ import PySimpleGUI as sg
 import cv2
 
 IMG_SIZE = 518
+
+regex = r'^Scene-(\d+)\.Frame-(\d+).(?:jpg|png)$'
 
 file_list_column = [
   [
@@ -75,10 +80,15 @@ def compare_images(image1, image2):
 def navigate_scenes(path_to_frames):
   for root, dirs, files in os.walk(path_to_frames):
     if len(files) > 0:
-      yield root, files
+      data = map(lambda file_name: (*(list(map(int, re.findall(regex, file_name)[0]))), file_name, root), filter(lambda x: len(re.findall(regex, x)) > 0, files))
+      for scene_n, frames in groupby(data, itemgetter(0)):
+        frames = list(frames)
+        f_files = list(map(itemgetter(2), frames))
+        c_root = frames[0][3] # all the root are the same
+        yield os.path.normpath(c_root), scene_n, f_files
 
 
-def move_images(root, files, output_dir):
+def move_images(root, file_dir, files, output_dir, files_name_update=None):
   prev = None
   valid_index = []
   for idx, image in enumerate(map(cv2.imread, map(lambda x: os.path.join(root, x), files))):
@@ -89,11 +99,14 @@ def move_images(root, files, output_dir):
     if not compare_images(prev, image):
       valid_index.append(idx)
       prev = image
-  output_root = root.replace(root.split(os.sep)[0], output_dir, 1)
+  output_root = os.path.join(output_dir, file_dir)
   if not os.path.exists(output_root):
     os.makedirs(output_root)
+  new_files_name = files
+  if files_name_update is not None:
+    new_files_name = list(map(files_name_update, files))
   for idx in valid_index:
-    shutil.copy(os.path.join(root, files[idx]), os.path.join(output_root, files[idx]))
+    shutil.copy(os.path.join(root, files[idx]), os.path.join(output_root, new_files_name[idx]))
 
 
 def extract_scene_number(root):
@@ -120,9 +133,9 @@ if __name__ == '__main__':
     event, values = window.read(timeout=75)
     # Folder name was filled in, make a list of files in the folder
     if event == "-FOLDER-IN-":
-      input_folder = values["-FOLDER-IN-"]
+      input_folder = os.path.normpath(values["-FOLDER-IN-"])
     elif event == "-FOLDER-OUT-":
-      output_folder = values["-FOLDER-OUT-"]
+      output_folder = os.path.normpath(values["-FOLDER-OUT-"])
     elif event == "-SCENE-":
       next_scene = int('0'+''.join(c for c in values["-SCENE-"] if c.isdigit()))
     elif event == "-START-":
@@ -130,22 +143,23 @@ if __name__ == '__main__':
       g = navigate_scenes(input_folder)
       window.write_event_value('-NEXT-', values)
       window["-START-"].update(disabled=True)
+      window.force_focus()
     elif event == "-NEXT-":
-      root_t, files = next(g, (None, None))
+      root_t, scene_n, files = next(g, (None, None, None))
       if root_t is None:
         window.close()
-        break
-      if root is not None and root_t.split(os.sep)[-2] != root.split(os.sep)[-2]:
         next_scene = 1
+        break
+      if root is not None and root_t.split(os.sep)[-1] != root.split(os.sep)[-1]:
+        next_scene = scene_n + 1
       root = root_t
-      window["-CURR-FILM-"].update(value=root.split(os.sep)[-2])
-      scene_n = extract_scene_number(root)
+      window["-CURR-FILM-"].update(value=root.split(os.sep)[-1])
       if scene_n >= next_scene:
         next_scene = scene_n + 1
       else:
         window.write_event_value('-NEXT-', values)
         continue
-      window["-CURR-FRAME-"].update(value=f'{next_scene}')
+      window["-CURR-FRAME-"].update(value=f'{scene_n}')
       files = sorted(files)
       window["-TOUT-1-"].update(files[0])
       window["-IMAGE-1-"].update(data=image_to_base64(os.path.join(root, files[0])))
@@ -153,7 +167,7 @@ if __name__ == '__main__':
     elif event is not None and (event.startswith('Right') or event == 'd' or event == 'y'):
       if root is not None and files is not None:
         print('Images accepted!')
-        move_images(root, files, output_folder)
+        move_images(root, str(window["-CURR-FILM-"].get()), files, output_folder)
         window.write_event_value('-NEXT-', values)
     elif event is not None and (event.startswith('Left') or event == 'a' or event == 'n'):
       if root is not None:
